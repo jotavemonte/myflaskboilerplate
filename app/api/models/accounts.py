5cabe29ac6499d68239a67cd5511f6ffb375b7a7
João Victor Monte <jotavemonte@gmail.com>
2019-07-07 10:20:36 -0300

    small adjustments

--- app/api/models/accounts.py
+++ app/api/models/accounts.py
@@ -5,8 +5,9 @@ from app.app import bcrypt
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    nickname = db.Column(db.String(255), unique=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_superuser = db.Column(db.Bool(), default=False)
+    is_superuser = db.Column(db.Bool(), default=False)
 
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password)
